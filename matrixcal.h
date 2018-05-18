5b13f489e465012da9ea9253fd5aba7cad73fc56
wuyuhao28 <wuyuhao28@gmail.com>
2018-05-18 19:05:25 +0800

    Update matrixcal.h

--- matrixcal.h
+++ matrixcal.h
@@ -9,8 +9,8 @@
 #include "memorypool.h"
 #include "memorypoolmanager.h"
 
-//#include <cublas_v2.h> //cuda×Ô´ø¿âº¯Êý
-#include <cublas.h>
+#include <cublas_v2.h> //cuda×Ô´ø¿âº¯Êý
+//#include <cublas.h>
 #include <sys/time.h>
 #include <time.h>
 #include <stdio.h>
