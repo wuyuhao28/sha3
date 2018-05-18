25f66d086530e4444f4e3fd2f8ff2b7ab65aefe6
wuyuhao28 <wuyuhao28@gmail.com>
2018-05-18 18:22:31 +0800

    Add files via upload

--- matrixcal.cu
+++ matrixcal.cu
@@ -221,7 +221,7 @@ cudaError_t matrixMul(Mat256x256i8& sourceMatrix, const Mat256x256i8* tmpMatrix,
 				&alpha, (void *)(matList + sequence[j] * matrixSize), CUDA_R_8I, 256,
 				(void *)tmp, CUDA_R_8I, 256,
 				&beta, (void *)source, CUDA_R_32I, 256,
-				CUDA_R_8I);
+				CUDA_R_8I, 0);
 			if (cublasSatus != CUBLAS_STATUS_SUCCESS)
 			{
 				printf("cublasSgemm_v2 error!, j: %d cublasError: %d\n", j, cublasSatus);
