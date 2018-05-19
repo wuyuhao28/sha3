5146c151ecc4013a362557b1ef6e42ba107de957
wuyuhao28 <wuyuhao28@gmail.com>
2018-05-19 18:12:17 +0800

    Update matrixcal.h

--- matrixcal.h
+++ matrixcal.h
@@ -9,7 +9,7 @@
 #include "memorypool.h"
 #include "memorypoolmanager.h"
 
-#include <cublas_v2.h> //cuda�Դ��⺯��
+#include <cublas_v2.h> //cuda×Ô´ø¿âº¯Êý
 //#include <cublas.h> 
 #include <sys/time.h>
 #include <time.h>
@@ -21,7 +21,7 @@ extern int8_t* g_device_matList[6];
 cudaError_t matrixMul(Mat256x256i8& sourceMatrix, const Mat256x256i8* tmpMatrix, const AlgriMatList* matList_int8, uint8_t *sequence);
 //__global__ void mulKernel(Mat256x256i8& sourceMatrix, Mat256x256i8* tmpMatrix, Mat256x256i8* seqMatrix);
 
-inline void iter(const uint8_t *msg, uint32_t len, uint8_t result[32], uint32_t threadID);
+void iter(const uint8_t *msg, uint32_t len, uint8_t result[32], uint32_t threadID);
 
 double GetMillsec();
 
