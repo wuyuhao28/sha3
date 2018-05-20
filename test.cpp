ceda0aefffa60b48c187ab2e75c8a7f06bb4a036
wuyuhao28 <wuyuhao28@gmail.com>
2018-05-20 16:33:59 +0800

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -120,6 +120,8 @@ int main(void)
     for (int i = 0;i<1 ; i++) {	
 
         iter(g_msg, 32, results,i);
+		end_t = GetMillsec();
+		printf("iter out time: %lf\n", end_t - start_t);
 
         int j = 0;
         for (; j < 32; j++) {
