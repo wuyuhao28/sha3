f8b5a0ec198115756094f8d51bcb0679e449cf57
wuyuhao28 <wuyuhao28@gmail.com>
2018-05-21 10:20:58 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -122,19 +122,19 @@ int main(void)
 
     for (int i = 0;i<1 ; i++) {	
 
-		//iter(g_msg, 32, results, i, res, mat, ctx);
-		iter();
+		iter(g_msg, 32, results, i, res, mat, ctx);
+		//iter();
 		end_t = GetMillsec();
 		printf("iter out time: %lf\n", end_t - start_t);
 
-        //int j = 0;
-        //for (; j < 32; j++) {
+        int j = 0;
+        for (; j < 32; j++) {
             // printf("0x%02x, ",results[i][j]);
-           // if (results[j] != g_results[j]) {
-				//printf("Results does not match, i: %d , j : %d \n", i, j);
-                //break;
-            //}
-        //}
+            if (results[j] != g_results[j]) {
+				printf("Results does not match, i: %d , j : %d \n", i, j);
+                break;
+            }
+        }
     }
 	//delete matList_int8;
 
